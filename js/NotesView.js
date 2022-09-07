2b50158d1519f7caeebf75b3a23813cc1b51b2b3
Sergey Shishin <flyers32rus@gmail.com>
2022-09-07 17:15:12 +0300

    finish basic logic work of app

--- js/NotesView.js
+++ js/NotesView.js
@@ -1,4 +1,5 @@
 export default class NotesView {
+    // Так мы делаем прототип для последующего размножения
     constructor ( root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}) {
         this.root = root;
         this.onNoteSelect = onNoteSelect;
@@ -21,10 +22,91 @@ export default class NotesView {
         const inpBody = this.root.querySelector('.notes__body');
 
         btnAddNote.addEventListener('click', () => {
-            this.onNoteAdd();
+            this.onNoteAdd();       //При клике выполниться вызов view.onNoteAdd()??? Где консоль лог
         });
+
+        // Сохранение при потере фокуса
+        [inpTitle,inpBody].forEach(inputField => {      //Перебираем массив
+            inputField.addEventListener('blur', () => {     //Когда поле потеряло фокус (не активно)
+                const updatedTitle = inpTitle.value.trim();     //Тогда получаем переданное значение в поля и без пробелов 
+                const updatedBody = inpBody.value.trim();
+                // console.log(updatedTitle)    //Проверка на то что записывается в переменную
+                // console.log(updatedBody)
+                this.onNoteEdit(updatedTitle, updatedBody);
+            });
+        });
+
+        this.updateNotePreviewVisibility(false);        //Скрываем по умполчанию текст заметки
+
+    }
+
+    _createListItemHTML(id, title, body, updated) {
+        const MAX_BODY_LENGTH = 60;
+
+        return `
+        <div class = "notes__list-item" data-note-id = "${id}">
+            <div class = "notes__small-title">${title}</div>
+            <div class = "notes__small-body">
+                ${body.substring(0, MAX_BODY_LENGTH)}
+                ${body.length > MAX_BODY_LENGTH ? '...' : ''}
+            </div>
+            <div class = "notes__small-updated">
+                ${updated.toLocaleString(undefined, { dataStyle: 'full', timeStyle: 'short'})}
+            </div>
+        </div>
+        `;  //Возвращаем строку в updated в определенном строчном формате даты, а аргументы этого метода это свойства объекта конкретного метода
+        
+    }
+
+    //Обновляем
+    updateNoteList(notes) {
+        const notesListContainer = this.root.querySelector('.notes__list');
+
+        //Очищаем содержимое
+        notesListContainer.innerHTML = '';    
+        
+        for (const note of notes) {
+            const html = this._createListItemHTML(note.id, note.title, note.body, new Date(note.updated));     //Присваиваем аргументы для рендера из объекта локального хранилища
+
+            notesListContainer.insertAdjacentHTML('beforeend', html);
+        }
+
+        notesListContainer.querySelectorAll('.notes__list-item').forEach(noteListItem => {
+            noteListItem.addEventListener('click', () => {
+                this.onNoteSelect(noteListItem.dataset.noteId);
+            });
+
+            noteListItem.addEventListener('dblclick', () => {
+                const doDelete = confirm('Are you sure you want to delete this note?');
+
+                if(doDelete) {
+                    this.onNoteDelete(noteListItem.dataset.noteId);
+                }
+            });
+        });
+    }
+
+
+    //Сразу отображаем заметку на главном экране готовой к редактированию
+    updateActiveNote(note) {
+        this.root.querySelector('.notes__title').value = note.title;        //Получаем определнную заметку из хранилища и присваиваем для разметки значение которое есть в хранилище для этой разметки
+        this.root.querySelector('.notes__body').value = note.body;
+
+        this.root.querySelectorAll('.notes__list-item').forEach(noteListItem => {
+            noteListItem.classList.remove('notes__list-item--selected');
+        });
+
+        this.root.querySelector(`.notes__list-item[data-note-id = "${note.id}"]`).classList.add('notes__list-item--selected');
+    }
+
+    updateNotePreviewVisibility(visible) {
+        this.root.querySelector('.notes__preview').style.visibility = visible ? 'visible' : 'hidden';
     }
 }
 
-// 27:20
+
+
+// 48:20
 // Расписать работу приложения
+// This отвечает за тот объект в контексте которого было вызвано
+// То есть в данном случае this.root = view.app
--- js/main.js
+++ js/main.js
@@ -16,11 +16,29 @@
 
 
 import NotesView from './NotesView.js';
+import NotesAPI from './NotesAPI.js';
 
-const app = document.getElementById("app");
-const view = new NotesView (app, {
-    onNoteAdd() {
+const app = document.getElementById("app");     //Получаем блок по ид
+// Просто создаем новый обьъект где контекстом вызова будет view, и переданные аргументы равны аргументам view, а в остальном это копия
+const view = new NotesView (app, {      //Создаем копию класса и помещаем в нее наш полученный блок
+    onNoteAdd() {       //Вызываем функцию которая вызовет консоль лог, а клик определен в модуле
         console.log('Lets add a note!');
+    }, 
+    onNoteSelect(id) {       //Выбираем 
+        console.log('Note selected: ' + id);
+    }, 
+    onNoteDelete(id) {       //Выбираем 
+        console.log('Note DELETED: ' + id);
+    }, 
+    onNoteEdit(newTitle, newBody) {     //Сохраняем написанный текст
+        // console.log('Save and updated');
+        console.log(newTitle);
+        console.log(newBody);
     }
+    
 });
 
+const notes = NotesAPI.getAllNotes();
+
+view.updateNoteList(notes);    //Перебираем полученный массив из локального хранилища для обновления и отображения на странице
+view.updateActiveNote(notes[0]);
